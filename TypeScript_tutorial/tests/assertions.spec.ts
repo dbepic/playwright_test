5b5042fdc81f6a9be5e8fc29ce56182668d32121
swethamaran1998-sudo <swethamaran1998@gmail.com>
2026-08-18 17:02:21 +0530

    Made chnages in assersion file

--- TypeScript_tutorial/tests/assertions.spec.ts
+++ TypeScript_tutorial/tests/assertions.spec.ts
@@ -2,7 +2,8 @@ import {test,expect} from '@playwright/test';
 test('locator',async({page})=>{
 //     page.goto
     await page.goto('https://demoqa.com/text-box');
-    await page.locator('#userName').fill('Swetha');
+    //Updated by Swetha
+    await page.locator('#userName').fill('Swetha Manimaran');
     await expect(page.locator('#userName')).toHaveValue('Swetha');
     await expect (page.locator("//h1")).toBeVisible();
     await expect(page).toHaveURL(/text-box/);
