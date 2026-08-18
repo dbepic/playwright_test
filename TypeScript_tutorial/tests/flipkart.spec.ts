8734a3d576889d85d545a775d4d190e9ca53740c
DevBharath-cyber <devbharathkathirvel@gmail.com>
2026-08-18 15:14:22 +0530

    new code

---
+++ TypeScript_tutorial/tests/flipkart.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+
+test('flipkart', async ({ page }) => {
+    await page.goto(`https://www.flipkart.com/`);
+
+    const title = await page.title();
+    console.log(`page title ${title}`);
+
+    const pageURL = await page.url();
+    console.log(`page url is ${pageURL}`);
+
+    await expect(page).toHaveTitle(`Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!`);
+    await expect(page).toHaveURL(`https://www.flipkart.com/`);
+
+    await page.close();
+})
+
+test('amazon', async ({ page }) => {
+    await page.goto(`https://www.amazon.in/`);
+
+    const title = await page.title();
+    console.log(`page title ${title}`);
+
+    const pageURL = await page.url();
+    console.log(`page url is ${pageURL}`);
+
+    await expect(page).toHaveTitle(` Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in
+`);
+    await expect(page).toHaveURL(`https://www.amazon.in/`);
+})
