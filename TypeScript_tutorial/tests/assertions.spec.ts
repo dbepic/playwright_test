b89a4bf6896114a89a64ff68e18330cb8d1f1932
swethamaran1998-sudo <swethamaran1998@gmail.com>
2026-08-18 17:21:48 +0530

    added new validation

--- TypeScript_tutorial/tests/assertions.spec.ts
+++ TypeScript_tutorial/tests/assertions.spec.ts
@@ -2,8 +2,7 @@ import {test,expect} from '@playwright/test';
 test('locator',async({page})=>{
 //     page.goto
     await page.goto('https://demoqa.com/text-box');
-    //Updated by Swetha Maran
-    await page.locator('#userName').fill('Sri');
+    await page.locator('#userName').fill('Swetha Manimaran');
     await expect(page.locator('#userName')).toHaveValue('Swetha');
     await expect (page.locator("//h1")).toBeVisible();
     await expect(page).toHaveURL(/text-box/);
