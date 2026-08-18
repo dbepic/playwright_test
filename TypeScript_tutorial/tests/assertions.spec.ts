b3cc2cbd8574812869ba0b378278859550a32684
swethamaran1998-sudo <swethamaran1998@gmail.com>
2026-08-18 17:04:59 +0530

    new changes

--- TypeScript_tutorial/tests/assertions.spec.ts
+++ TypeScript_tutorial/tests/assertions.spec.ts
@@ -2,8 +2,8 @@ import {test,expect} from '@playwright/test';
 test('locator',async({page})=>{
 //     page.goto
     await page.goto('https://demoqa.com/text-box');
-    //Updated by Swetha
-    await page.locator('#userName').fill('Swetha Manimaran');
+    //Updated by Swetha Maran
+    await page.locator('#userName').fill('Sri');
     await expect(page.locator('#userName')).toHaveValue('Swetha');
     await expect (page.locator("//h1")).toBeVisible();
     await expect(page).toHaveURL(/text-box/);
