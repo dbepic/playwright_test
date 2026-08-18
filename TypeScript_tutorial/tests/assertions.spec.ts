771bccb5fd8395e4379692e8d6a2ed98be809312
Pragadesh1804 <pragadeesh18pragi@gmail.com>
2026-08-18 17:35:29 +0530

    dev

--- TypeScript_tutorial/tests/assertions.spec.ts
+++ TypeScript_tutorial/tests/assertions.spec.ts
@@ -1,9 +1,9 @@
-import {test,expect} from '@playwright/test';
+import {test,expect} from '@playwright/test';]
 test('locator',async({page})=>{
 //     page.goto
     await page.goto('https://demoqa.com/text-box');
-    await page.locator('#userName').fill('Swetha');
-    await expect(page.locator('#userName')).toHaveValue('Swetha');
+    await page.locator('#userName').fill('dev');
+    await expect(page.locator('#userName')).toHaveValue('dev');
     await expect (page.locator("//h1")).toBeVisible();
     await expect(page).toHaveURL(/text-box/);
     
