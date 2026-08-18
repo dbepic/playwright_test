164e340113f8bd749d7cf8fb6e21eaa11ededa02
Pragadesh1804 <pragadeesh18pragi@gmail.com>
2026-08-18 17:46:03 +0530

    new feacture

---
+++ tests/amazon.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test'
+
+
+test('amazon', async ({ page }) => {
+    await page.goto(`https://www.amazon.in/`);
+
+    const title = await page.title();
+    console.log(`page title ${title}`);
+
+    const pageURL = await page.url();
+    console.log(`page url is ${pageURL}`);
+
+    await expect(page).toHaveTitle(` Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in
+`);
+    await expect(page).toHaveURL(`https://www.amazon.in/`);
+})
