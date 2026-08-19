1ec76bd812bc770db38e8c27a3a1fe29a8cedd9f
DevBharath-cyber <devbharathkathirvel@gmail.com>
2026-08-19 15:05:03 +0530

    new changes

---
+++ TypeScript_tutorial/tests/assertions.spec.ts
@@ -0,0 +1,10 @@
+import {test,expect} from '@playwright/test';]
+test('locator',async({page})=>{
+//     page.goto
+    await page.goto('https://demoqa.com/text-box');
+    await page.locator('#userName').fill('dev');
+    await expect(page.locator('#userName')).toHaveValue('dev');
+    await expect (page.locator("//h1")).toBeVisible();
+    await expect(page).toHaveURL(/text-box/);
+    
+});
