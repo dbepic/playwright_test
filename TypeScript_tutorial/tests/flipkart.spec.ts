8443ba82d72cb7510058902c8b9f67559760664b
DevBharath-cyber <devbharathkathirvel@gmail.com>
2026-08-21 12:09:30 +0530

    new code

--- TypeScript_tutorial/tests/flipkart.spec.ts
+++ TypeScript_tutorial/tests/flipkart.spec.ts
@@ -17,7 +17,7 @@ test('flipkart', async ({ page }) => {
 })
 
 test('amazon', async ({ page }) => {
-    await page.goto(`https://www.amazon.in/`);
+    await page.goto(`https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=155259815513&hvpone=&hvptwo=&hvadid=815461303151&hvpos=&hvnetw=g&hvrand=2867626201928924589&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9197818&hvtargid=kwd-10573980&hydadcr=14453_2462831&mcid=4c22dcdee2bf3a71b0b832c5c4ba9c17&hvocijid=2867626201928924589--&hvexpln=nav&gad_source=1`);
 
     const title = await page.title();
     console.log(`page title ${title}`);
@@ -27,5 +27,5 @@ test('amazon', async ({ page }) => {
 
     await expect(page).toHaveTitle(` Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in
 `);
-    await expect(page).toHaveURL(`https://www.amazon.in/`);
+    await expect(page).toHaveURL(`https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=155259815513&hvpone=&hvptwo=&hvadid=815461303151&hvpos=&hvnetw=g&hvrand=2867626201928924589&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9197818&hvtargid=kwd-10573980&hydadcr=14453_2462831&mcid=4c22dcdee2bf3a71b0b832c5c4ba9c17&hvocijid=2867626201928924589--&hvexpln=nav&gad_source=1`);
 })
